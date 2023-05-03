de66c96cbd6b50e224a505d53fa8706a90ec6a25
Robert Rumney <robertrumney@gmail.com>
2023-05-03 18:40:54 +0200

    Update WindManager.cs

--- WindManager.cs
+++ WindManager.cs
@@ -6,17 +6,33 @@ public class WindManager : MonoBehaviour
 {
     public float windForce = 10.0f;
     public Vector3 windDirection = Vector3.right;
+    public bool dynamicRigidbodies = true;
+
+    private List<Rigidbody> cachedRigidbodies;
+
+    private void Start()
+    {
+        if (!dynamicRigidbodies)
+        {
+            cacheRigidbodies();
+        }
+    }
 
     private void FixedUpdate()
     {
-        Rigidbody[] allRigidbodies = GameObject.FindObjectsOfType<Rigidbody>();
+        List<Rigidbody> allRigidbodies = dynamicRigidbodies ? new List<Rigidbody>(GameObject.FindObjectsOfType<Rigidbody>()) : cachedRigidbodies;
 
         foreach (Rigidbody rb in allRigidbodies)
         {
-            if (!rb.isKinematic)
+            if (rb != null && !rb.isKinematic)
             {
                 rb.AddForce(windDirection.normalized * windForce);
             }
         }
+    }
+
+    private void CacheRigidbodies()
+    {
+        cachedRigidbodies = new List<Rigidbody>(GameObject.FindObjectsOfType<Rigidbody>());
     }
 }
